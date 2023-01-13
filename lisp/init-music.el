32d8c33e4402743c682e8854e440e6516302f1e9
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-01-12 20:59:52 -0600

    Ensure mpv is on path before config

--- lisp/init-music.el
+++ lisp/init-music.el
@@ -5,16 +5,16 @@
 ;; (require-package 'bongo)
 ;; (setq bongo-enabled-backends '(mpv))
 ;; (setq bongo-mode-line-indicator-function 'ignore)
+(when (executable-find "mpv")
+  (require-package 'mpv)
 
-;; Check that mpv exists on path, like purcell
-(require-package 'mpv)
+  (defun sujay/play-on-loop (path)
+    "Play a song on loop."
+    (interactive
+     (list (read-file-name "Pick a song: " "~/audio/")))
+    (mpv-play path)
+    (mpv-set-property "loop-file" "inf")))
 
-(defun sujay/play-on-loop (path)
-  "Play a song on loop."
-  (interactive
-   (list (read-file-name "Pick a song: " "~/audio/")))
-  (mpv-play path)
-  (mpv-set-property "loop-file" "inf"))
 
 (require-package 'ytdl)
 (setq ytdl-music-folder "~/audio/"
