df6e7919d31bfc85a91f9e7843b3b09e7db945a7
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-01-24 15:37:08 -0600

    remove day abbrevs for org compatability

--- lisp/init-abbrev.el
+++ lisp/init-abbrev.el
@@ -9,13 +9,6 @@
 (define-abbrev text-mode-abbrev-table "wo" "without")
 (define-abbrev text-mode-abbrev-table "bw" "between")
 (define-abbrev text-mode-abbrev-table "tmrw" "tomorrow")
-(define-abbrev text-mode-abbrev-table "sun" "sunday")
-(define-abbrev text-mode-abbrev-table "mon" "monday")
-(define-abbrev text-mode-abbrev-table "tue" "tuesday")
-(define-abbrev text-mode-abbrev-table "wed" "wednesday")
-(define-abbrev text-mode-abbrev-table "thu" "thursday")
-(define-abbrev text-mode-abbrev-table "fri" "friday")
-(define-abbrev text-mode-abbrev-table "sat" "saturday")
 (define-abbrev text-mode-abbrev-table "ppl" "people")
 
 ;; My Abbreviations
