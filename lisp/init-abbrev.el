bb411baa2f1dcaa7d360082d4866ec9a9cd18389
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-01-26 20:31:21 -0600

    Abbrevs

--- lisp/init-abbrev.el
+++ lisp/init-abbrev.el
@@ -8,6 +8,7 @@
 (define-abbrev text-mode-abbrev-table "w" "with")
 (define-abbrev text-mode-abbrev-table "wo" "without")
 (define-abbrev text-mode-abbrev-table "bw" "between")
+(define-abbrev text-mode-abbrev-table "bc" "because")
 (define-abbrev text-mode-abbrev-table "tmrw" "tomorrow")
 (define-abbrev text-mode-abbrev-table "ppl" "people")
 (define-abbrev text-mode-abbrev-table "hw" "homework")
@@ -16,18 +17,23 @@
 ;; My Abbreviations
 (define-abbrev text-mode-abbrev-table "apt" "apartment")
 (define-abbrev text-mode-abbrev-table "appt" "appointment")
+(define-abbrev text-mode-abbrev-table "alg" "algorithm")
+(define-abbrev text-mode-abbrev-table "gov" "government")
+(define-abbrev text-mode-abbrev-table "esp" "especially")
 (define-abbrev text-mode-abbrev-table "bb" "basketball")
 (define-abbrev text-mode-abbrev-table "vb" "volleyball")
 (define-abbrev text-mode-abbrev-table "tt" "table tennis")
 (define-abbrev text-mode-abbrev-table "oo" "object oriented")
 (define-abbrev text-mode-abbrev-table "ood" "object oriented design")
 (define-abbrev text-mode-abbrev-table "oop" "object oriented programming")
+(define-abbrev text-mode-abbrev-table "ai" "artificial intelligence")
 (define-abbrev text-mode-abbrev-table "ml" "machine learning")
 (define-abbrev text-mode-abbrev-table "inv" "investigate")
 (define-abbrev text-mode-abbrev-table "oa" "online assessment")
 (define-abbrev text-mode-abbrev-table "gb" "grand blue")
 (define-abbrev text-mode-abbrev-table "q" "question")
-(define-abbrev text-mode-abbrev-table "y" "why")
+(define-abbrev text-mode-abbrev-table "doc" "document")
+
 
 ;; Typos
 (define-abbrev text-mode-abbrev-table "ot" "to")
