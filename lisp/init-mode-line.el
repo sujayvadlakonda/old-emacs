fb8749a1ccbb086d397ff2884666129ef9c09ae9
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-02-19 11:43:22 -0600

    Formatting

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -1,10 +1,4 @@
-;;; init-mode-line.el --- Configure mode line -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
 (setq-default line-number-mode nil)
 (setq-default column-number-mode nil)
 
-
 (provide 'init-mode-line)
-;;; init-mode-line.el ends here
