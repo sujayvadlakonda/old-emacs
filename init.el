75621c381386f7bd558dc56fbb154e688e7b283a
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-02-21 15:56:12 -0600

    Gain insight into garbage collection

--- init.el
+++ init.el
@@ -24,11 +24,10 @@
 
 ;; Adjust garbage collection thresholds during startup, and thereafter
 
-(let ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024)))
-  (setq gc-cons-threshold init-gc-cons-threshold)
-  (add-hook 'emacs-startup-hook
-            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+(setq garbage-collection-messages t)
+(setq gc-cons-threshold (* 128 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda () (setq gc-cons-threshold (* 20 1024 1024))))
 
 
 ;; Bootstrap config
