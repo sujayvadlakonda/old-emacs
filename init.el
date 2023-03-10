7124b15e40f2f44dda8dd33ce9bb61b41fde1c94
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-09 23:17:07 -0600

    Remove emacs version warnings

--- init.el
+++ init.el
@@ -6,11 +6,6 @@
     (load (symbol-name feature))
     (provide feature)))
 
-(let ((minver "26.1"))
-  (when (version< emacs-version minver)
-    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
-(when (version< emacs-version "27.1")
-  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking)
