5b2c0eca50ebe403cf93350533ccced982e423e6
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-12 17:28:09 -0500

    formatting

--- lisp/init-scratch.el
+++ lisp/init-scratch.el
@@ -1,15 +1,7 @@
-;;; init-scratch.el --- Insert description here -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-;; (require-package 'immortal-scratch)
-;; (add-hook 'after-init-hook 'immortal-scratch-mode)
+;; -*- lexical-binding: t -*-
 
 (defun remove-scratch-buffer ()
   (when (get-buffer "*scratch*")
     (kill-buffer "*scratch*")))
 
 (add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
-
-(provide 'init-scratch)
-;;; init-scratch.el ends here
