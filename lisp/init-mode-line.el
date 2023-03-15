f805bfbeb30760e8061f9f69e353e4519292bc8f
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-15 10:56:47 -0500

    commentary

--- lisp/init-mode-line.el
+++ lisp/init-mode-line.el
@@ -1,4 +1,7 @@
 ;; -*- lexical-binding: t -*-
 
+;; Rely on OS for time and date
+;; You don't want the time in each window split
+
 (setq-default line-number-mode nil)
 (setq-default column-number-mode nil)
