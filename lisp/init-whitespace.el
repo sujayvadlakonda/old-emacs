f1dc66b05bbd780889eae293655fe8b09fc7b6e4
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-16 21:50:15 -0500

    don't show whitespace

--- lisp/init-whitespace.el
+++ lisp/init-whitespace.el
@@ -1,9 +1,10 @@
-(setq-default show-trailing-whitespace t)
 ;; -*- lexical-binding: t; -*-
 
 ;; Remove trailing whitespace on save
 ;; Only in lines edited (prevent noisy git commits)
 ;; While preserving spaces in buffer (prevent cursor jumping on autosave)
+
+(setq-default show-trailing-whitespace nil)
 
 ;; I only hooked prog-mode to preserve the virtual space
 ;; in capture buffers with auto-save-visited-mode
