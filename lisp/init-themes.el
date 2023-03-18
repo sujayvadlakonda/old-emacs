b37caa4df88560146dbb86ae0ad5d44a22b57d73
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-17 22:21:03 -0500

    refactor

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -42,19 +42,17 @@
   (reapply-themes))
 
 
-(when (maybe-require-package 'dimmer)
+;; Don't dim in terminal windows. Even with 256 colours it can
+;; lead to poor contrast.  Better would be to vary dimmer-fraction
+;; according to frame type.
+(when (display-graphic-p)
+  (require-package 'dimmer)
   (setq-default dimmer-fraction 0.15)
   (add-hook 'after-init-hook 'dimmer-mode)
   (with-eval-after-load 'dimmer
     ;; TODO: file upstream as a PR
-    (advice-add 'frame-set-background-mode :after (lambda (&rest args) (dimmer-process-all))))
-  (with-eval-after-load 'dimmer
-    ;; Don't dim in terminal windows. Even with 256 colours it can
-    ;; lead to poor contrast.  Better would be to vary dimmer-fraction
-    ;; according to frame type.
-    (defun sanityinc/display-non-graphic-p ()
-      (not (display-graphic-p)))
-    (add-to-list 'dimmer-exclusion-predicates 'sanityinc/display-non-graphic-p)))
+    (advice-add 'frame-set-background-mode :after (lambda (&rest args) (dimmer-process-all)))))
+
 
 ;; Don't stack themes. Load them one at a time by default.
 (defun load-theme--disable-old-theme (theme &rest args)
