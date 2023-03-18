34bca3a4b645f10dd46551a8a50ca48d3dd0ac0c
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-18 12:47:31 -0500

    commentary

--- lisp/init-emacs-lisp.el
+++ lisp/init-emacs-lisp.el
@@ -1,5 +1,12 @@
 ;; -*- lexical-binding: t -*-
 
+;; Make working with emacs-lisp pleasant and simple
+;; Not for creating ELPA packages
+
+;; Use `elisp-slime-nav-describe-elisp-thing-at-point' for help documentation
+;; Use `elisp-slime-nav-find-elisp-thing-at-point' to jump to definition
+;; Use `pop-tag-mark' to jump back
+
 (require-package 'elisp-slime-nav)
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
