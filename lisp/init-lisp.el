2b670d516bd31bd347313ca498d50eb4fb3b3720
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-03-18 12:49:22 -0500

    don't appease flycheck

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -4,19 +4,6 @@
 
 (setq-default debugger-bury-or-kill 'kill)
 
-(defun sanityinc/headerise-elisp ()
-  "Add minimal header and footer to an elisp buffer in order to placate flycheck."
-  (interactive)
-  (let ((fname (if (buffer-file-name)
-                   (file-name-nondirectory (buffer-file-name))
-                 (error "This buffer is not visiting a file"))))
-    (save-excursion
-      (goto-char (point-min))
-      (insert ";;; " fname " --- Insert description here -*- lexical-binding: t -*-\n"
-              ";;; Commentary:\n"
-              ";;; Code:\n\n")
-      (goto-char (point-max))
-      (insert ";;; " fname " ends here\n"))))
 
 
 ;; Make C-x C-e run 'eval-region if the region is active
