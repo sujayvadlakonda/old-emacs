749002bdfae433272823a482d9bc014f7ffe604d
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-05-11 13:24:01 -0500

    Remove some highlighting for org-agenda

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -60,6 +60,15 @@
   (add-hook 'org-agenda-mode-hook
             (lambda () (add-hook 'window-configuration-change-hook 'org-agenda-align-tags nil t))))
 
+;; No face for weekends
+(setq org-agenda-weekend-days nil)
+
+;; No face for mouse highlighting
+(add-hook 'org-agenda-finalize-hook
+          (lambda () (remove-text-properties
+                      (point-min) (point-max) '(mouse-face t))))
+
+
 
 
 
