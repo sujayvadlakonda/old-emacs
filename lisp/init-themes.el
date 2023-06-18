66a1f6446744f48c519db5cccea7a12b67e9410b
Sujay Vadlakonda <sujay.vadlakonda@gmail.com>
2023-06-17 22:03:57 -0500

    Use modus themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -2,44 +2,25 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'color-theme-sanityinc-solarized)
-(require-package 'color-theme-sanityinc-tomorrow)
-
 ;; Don't prompt to confirm theme safety. This avoids problems with
 ;; first-time startup on Emacs > 26.3.
 (setq custom-safe-themes t)
 
-;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+(defvar sujay/active-theme nil "The active theme.")
 
-;; Ensure that themes will be applied even if they have not been customized
-(defun reapply-themes ()
-  "Forcibly load the themes listed in `custom-enabled-themes'."
-  (dolist (theme custom-enabled-themes)
-    (unless (custom-theme-p theme)
-      (load-theme theme)))
-  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+(defun sujay/load-theme (theme)
+  "Load a single theme."
+  (interactive
+   (list
+    (intern (completing-read "Load custom theme: "
+                             (mapcar #'symbol-name
+                                     (custom-available-themes))))))
+  (disable-theme 'sujay/active-theme)
+  (load-theme theme)
+  (setq sujay/active-theme theme))
 
-(add-hook 'after-init-hook 'reapply-themes)
-
-
-
-;; Toggle between light and dark
-
-(defun light ()
-  "Activate a light color theme."
-  (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-day))
-  (reapply-themes))
-
-(defun dark ()
-  "Activate a dark color theme."
-  (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
-  (reapply-themes))
-
-
-
+(require-package 'modus-themes)
+(sujay/load-theme 'modus-vivendi-tinted)
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
